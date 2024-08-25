3c40603f5c7a3139a9b1f13499012aaddc695929
Action0358 <dai03588010@gmail.com>
2024-08-25 03:55:22 +0000

    edit comment out

--- Beginner.go/Lesson01/lesson01.go
+++ Beginner.go/Lesson01/lesson01.go
@@ -1,8 +1,8 @@
-package main
+package main // Goのプログラムが実行される起点となるような特別なパッケージ
 
-import "fmt"
+import "fmt" // fmt パッケージに用意された Print 系関数
 
-func main() {
+func main() { // main関数を宣言
 	var num int      // var=変数宣言　num=変数名を記述　int=変数に代入するデータの種類
 	num = 1          // 変数に引数を代入
 	fmt.Println(num) // 変数に代入した値を表示
