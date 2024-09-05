daca6a418bf0115f494209f22457c9445c4096f6
Action0358 <dai03588010@gmail.com>
2024-09-05 16:14:59 +0000

    edit comment out

--- Beginner.go/Lesson38/lesson38.go
+++ Beginner.go/Lesson38/lesson38.go
@@ -6,7 +6,7 @@ func main() {
 	// deferで無名関数を定義し、main関数が終了する際に実行する処理を予約
 	defer func() {
 		// recover関数でパニック状態から回復を試みる
-		if x := recover(); x != nil { // xがnilでない場合、つまりパニックが発生している場合
+		if x := recover(); x != nil { // recover() がパニックメッセージをキャッチし、x に "runtime error" が代入される
 			fmt.Println(x) // パニックの内容（今回の場合は"runtime error"）を出力
 		}
 	}()
