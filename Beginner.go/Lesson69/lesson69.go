8c402fd33912796af48caacfaf6ebf332c9af798
Action0358 <dai03588010@gmail.com>
2024-09-22 09:00:45 +0000

    Edit 2 files

--- Beginner.go/Lesson69/lesson69.go
+++ Beginner.go/Lesson69/lesson69.go
@@ -9,8 +9,7 @@ type Point struct {
 }
 
 // String() メソッドで Point 型のカスタム文字列表現を返す。
-// fmt.Stringer インターフェースを実装している。
-func (p *Point) String() string {
+func (p *Point) String() string { // レシーバによって main 関数の p がメソッドを呼び出せるようになっている
 	return fmt.Sprintf("<<%v, %v>>", p.A, p.B) // Sprintf はフォーマットされた文字列を生成するために使われ、出力しない
 }
 
@@ -19,3 +18,8 @@ func main() {
 	p := &Point{100, "ABC"}
 	fmt.Println(p) // "<<100, ABC>>" と出力される
 }
+
+// p は Point 型のポインタを指す変数（12, 18行目）
+// *Point はそのポインタが指している元の Point 構造体のデータを操作するための型指定（19行目のデータ）
+// p := &Point{100, "ABC"} は Point 構造体を生成し、そのアドレス（ポインタ）の取得を行う
+// fmt.Println 関数が p に対して String() メソッドを呼び出して、main関数内でフォーマットされた文字列が出力される
